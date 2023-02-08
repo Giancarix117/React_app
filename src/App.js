4104f0fd8a58a6ce3d2f5384a6d0568a099e71f2
Giancarix117 <giancarlobetti2008@hotmail.com>
2023-02-08 10:38:07 +0100

    removed prop tyypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Button from "./Button";
-import Clicky from "./anothabutton";
 
 const App = ({ value = 0 }) => {
   const [count, setCount] = React.useState(0);
