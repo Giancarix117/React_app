ce372cdbcda59205e568af09c15af08b70b0e75e
Giancarix117 <giancarlobetti2008@hotmail.com>
2023-02-08 09:57:58 +0100

    lint staged

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
+import * as React from "react";
 import "@testing-library/jest-dom";
 
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
@@ -10,14 +11,5 @@ it("number gets increased with click", async () => {
   expect(getByTestId("counter-result")).toHaveTextContent("0");
   userEvent.click(getByTestId("counter-button"));
   expect(getByTestId("counter-starting-value")).toHaveTextContent("0");
-  expect(getByTestId("counter-result")).toHaveTextContent("1");
-});
-
-it("should handle initial value correctly", async () => {
-  const { getByTestId } = render(<App value={2} />);
-  expect(getByTestId("counter-starting-value")).toHaveTextContent("2");
-  expect(getByTestId("counter-result")).toHaveTextContent("0");
-  userEvent.click(getByTestId("counter-button"));
-  expect(getByTestId("counter-starting-value")).toHaveTextContent("2");
   expect(getByTestId("counter-result")).toHaveTextContent("1");
 });
